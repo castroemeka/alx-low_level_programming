b5b91adb74b8559c8bdffd8bb2cbb11539089a93
castroemeka <castro.emeka@gmail.com>
2023-09-14 03:23:10 -0700

    bugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void postive_or_negative(int i);
+{
+if (i > 0)
+{
+        printf("%dd is postive\n", i);
+}
+else if (i == 0)
+                printf("%d is zero\n", i);
+else
+        printf("%d is negative\n", i);
+}
+~
+~
+
