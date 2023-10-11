6f109e9051d9bbd8e3e5e3eeed610952cc5a1d0d
castroemeka <castro.emeka@gmail.com>
2023-10-11 13:21:15 -0700

    many33

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,31 +3,30 @@
 #include <string.h>
 
 /**
- * get_op_func - ...
- *
- * @s: ...
- * Return: ...
- */
-
-
+  * get_op_func - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
 int (*get_op_func(char *s))(int, int)
 {
-		op_t ops[] = {
-				{ "+", op_add },
-					{ "-", op_sub },
-						{ "*", op_mul },
-							{ "/", op_div }
-					{ "%", op_mod },
-							{ NULL, NULL }
-						};
-			int i = 0;
+	op_t ops[] = {
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
+	};
+	int i = 0;
 
-				while (i < 5)
-						{
-									if (strcmp(s, ops[i].op) == 0)
-													return (ops[i].f);	
-											i++;
-												}
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
 
-					return (0);
+		i++;
+	}
+
+	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,100 +3,73 @@
 #include <stdio.h>
 
 /**
- * op_add - ...
- *
- * @a: ...
- *
- * @b: ...
- *
- * Return: ...
- *
- */
-
-
+  * op_add - add numbers
+  * @a: first number
+  * @b: second number
+  *
+  * Return: addition
+  */
 int op_add(int a, int b)
 {
-		return (a + b);
+	return (a + b);
 }
 
 /**
- * op_sub - ...
- *
- * @a: ...
- *
- * @b: ...
- *
- *
- * Return: ...
- *
- */
-
-
+  * op_sub - subtract numbers
+  * @a: first number
+  * @b: second number
+  *
+  * Return: subtraction
+  */
 int op_sub(int a, int b)
 {
-		return (a - b);
+	return (a - b);
 }
 
 /**
- * op_mul - ...
- *
- * @a: ...
- *
- * @b: ...
- *
- *  Return: ...
- *
- */
-
-
+  * op_mul - multiply 2 numbers
+  * @a: first number
+  * @b: second number
+  *
+  * Return: multiplication
+  */
 int op_mul(int a, int b)
 {
-		return (a * b);
+	return (a * b);
 }
 
 /**
- * op_div - ...
- *
- * @a: ...
- *
- * @b: ...
- *
- *
- *  Return: ...
- */
-
-
+  * op_div - divide two numbers
+  * @a: first number
+  * @b: second number
+  *
+  * Return: division
+  */
 int op_div(int a, int b)
 {
-		if (b == 0)
-				{
-							printf("Error\n");
-									exit(100);
-										}
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-			return (a / b);
+	return (a / b);
 }
 
 /**
- *
- * op_mod - ...
- *
- * @a: ...
- *
- * @b: ...
- *
- * Return: ...
- */
-
-
-
+  * op_mod - modulations
+  * @a: first number
+  * @b: second number
+  *
+  * Return: modulation
+  */
 int op_mod(int a, int b)
 {
-		if (b == 0)
-				{
-							printf("Error\n");
-									exit(100);
-										}
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-			return (a % b);
+	return (a % b);
 }
